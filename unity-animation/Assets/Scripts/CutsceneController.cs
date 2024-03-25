05ebba2efd5c52471a09715b657e8ed4d5653e1c
jbees918 <josh.beestoned@gmail.com>
2024-03-25 06:20:53 -0500

    ty

---
+++ unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject Player;
+    public GameObject timerCanvas;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        animator.Play("Level01");
+    }
+
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Intro01") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            mainCamera.SetActive(true); 
+            Player.SetActive(true);
+            timerCanvas.SetActive(true);
+            this.enabled = false;
+        }
+    }
+}
