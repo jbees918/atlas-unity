08bda18e48c59a2b59dffae9edac73b432bd881c
jbees918 <josh.beestoned@gmail.com>
2024-05-05 03:54:03 -0500

    anim proj

--- unity-animation/Assets/PlayerController.cs
+++ unity-animation/Assets/PlayerController.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+    public float rotationSpeed = 4f;
     public float speed = 7f;
+    private CharacterController characterController;
+    
+    private Camera mainCamera; // Reference to the main camera
+    private Vector3 forwardDirection; // Forward direction of the camera
+
+    
+    private Vector3 startPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        startPosition = transform.position;
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -18,7 +32,6 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection *= speed * Time.deltaTime;
-        transform.Translate(movementDirection * Time.deltaTime);
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
     }
 }
